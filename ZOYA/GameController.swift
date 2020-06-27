81059ce22864cfa173b5566d9feae6d4e88c9383
Gaby Ardi <gabriella.ardiles12@gmail.com>
2020-06-27 14:32:10 -0400

    New functions, fixed welcome page, working on paranoia

--- ZOYA/GameController.swift
+++ ZOYA/GameController.swift
@@ -16,14 +16,8 @@ class GameController: UIViewController {
     //2 = WHO (purple)
     //3 = misc (blue)
     //4 = ToD (red)
-    var deck = [["1", "if you're wearing jeans"],
-                ["1", "if you have an allergy"],
-                ["2", "who will get married last"],
-                ["2", "who is least athletic"],
-                ["3", "play a round of spin the bottle"],
-                ["3", "give out a compliment and an insult"],
-                ["4", "tell the story of how you lost your virginity"],
-                ["4", "who is the least attractive"]]
+    
+    var deck = [[String]]()
     
     let colors = [[UIColor.init(displayP3Red: 0.15, green: 0.35, blue: 0.15, alpha: 1), UIColor.init(displayP3Red: 0.85, green: 1, blue: 0.85, alpha: 1)], [UIColor.init(displayP3Red: 0.35, green: 0.15, blue: 0.35, alpha: 1), UIColor.init(displayP3Red: 1, green: 0.85, blue: 1, alpha: 1)], [UIColor.init(displayP3Red: 0.15, green: 0.15, blue: 0.35, alpha: 1), UIColor.init(displayP3Red: 0.85, green: 0.85, blue: 1, alpha: 1)], [UIColor.init(displayP3Red: 0.35, green: 0.15, blue: 0.15, alpha: 1), UIColor.init(displayP3Red: 1, green: 0.85, blue: 0.85, alpha: 1)], [UIColor.black, UIColor.white]]
     
@@ -35,10 +29,52 @@ class GameController: UIViewController {
     
     @IBOutlet weak var card: UILabel!
     
+    func makeDeck(fname: String){
+        let fileURLProject = Bundle.main.path(forResource: fname, ofType: "txt")
+               var readStringProject = ""
+               do {
+                   readStringProject = try String(contentsOfFile: fileURLProject!, encoding:String.Encoding.utf8)
+               } catch let error as NSError {
+                   //print("Failed to read file")
+                   //print(error)
+               }
+               let lines = readStringProject.components(separatedBy: "\n")
+               var attribute = [[String]]()
+               for item in lines {
+                   //print(item.components(separatedBy: ":"))
+                   attribute.append(item.components(separatedBy: ":"))
+               }
+               attribute.remove(at: attribute.count-1)
+               deck += attribute
+               
+    }
+    
+    /*
+     func paranoiaDeck(Deck: [[String]]) -> [String] {
+        var subDeck = [[String]] ()
+        for card in Deck{
+        if card[0] == labels[1]{
+            subDeck.append(contentOf: [card])
+            }
+        print(subDeck)
+        return subDeck
+     }
+     */
+    
     override func viewDidLoad() {
         //print(truthtable)
         
         super.viewDidLoad()
+        makeDeck(fname: "deck")
+        for i in 1...truthtable.count {
+            let ex = truthtable[i-1]
+            if ex {
+                makeDeck(fname: String(i))
+            }
+        }
+        let index = Int.random(in: 0...(deck.count-1))
+        currentcard = deck[index]
+        deck.remove(at: index)
         card.text = currentcard[1]
         let kind = (Int(currentcard[0]) ?? 4) - 1
         //print(kind)
@@ -46,49 +82,13 @@ class GameController: UIViewController {
         category.textColor = colors[kind][1]
         category.text = labels[kind]
         card.textColor = colors[kind][1]
-        
+        if currentcard.count == 3 {
+                   if currentcard[2] == "paranoia"{
+                       //run paranoia Deck function
+                        print("Use who Deck")
+                   }
+               }
 
-        let fileURLProject = Bundle.main.path(forResource: "deck", ofType: "txt")
-        var readStringProject = ""
-        do {
-            readStringProject = try String(contentsOfFile: fileURLProject!, encoding:String.Encoding.utf8)
-        } catch let error as NSError {
-            //print("Failed to read file")
-            //print(error)
-        }
-        let lines = readStringProject.components(separatedBy: "\n")
-        var attribute = [[String]]()
-        for item in lines {
-            //print(item.components(separatedBy: ":"))
-            attribute.append(item.components(separatedBy: ":"))
-        }
-        attribute.remove(at: attribute.count-1)
-        deck = attribute
-        
-        for i in 1...truthtable.count {
-            let ex = truthtable[i-1]
-            if ex {
-                let currfileURLProject = Bundle.main.path(forResource: String(i), ofType: "txt")
-                var currreadStringProject = ""
-                do {
-                    currreadStringProject = try String(contentsOfFile: currfileURLProject!, encoding:String.Encoding.utf8)
-                } catch let error as NSError {
-                    print("Failed to read file")
-                    print(error)
-                }
-                let lines = currreadStringProject.components(separatedBy: "\n")
-                //var attribute = [[String]]()
-                for item in lines {
-                    //print(item.components(separatedBy: ":"))
-                    deck.append(item.components(separatedBy: ":"))
-                }
-                deck.remove(at: deck.count-1)
-                //deck.append(attribute)
-                
-            }
-        }
-
-        // Do any additional setup after loading the view.
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -115,6 +115,13 @@ class GameController: UIViewController {
         category.textColor = colors[kind][1]
         category.text = labels[kind]
         card.textColor = colors[kind][1]
+        
+        if currentcard.count == 3 {
+            if currentcard[2] == " paranoia"{
+                print("Use who Deck")
+                //run paranoia Deck function
+            }
+        }
         //
     }
     /*
